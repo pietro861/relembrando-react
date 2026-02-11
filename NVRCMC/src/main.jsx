13c2fa64d77c7f80978435c15517d005deb2c420
Luiz Otávio <luiz.fontenele.0@gmail.com>
2026-02-11 11:28:12 -0300

    Bugfixes da merge feita pelo danillo

--- NVRCMC/src/main.jsx
+++ NVRCMC/src/main.jsx
@@ -2,7 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router';
+import { BrowserRouter } from 'react-router-dom'
+import 'primereact/resources/themes/lara-light-indigo/theme.css'
+import 'primereact/resources/primereact.min.css'
+import 'primeicons/primeicons.css'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
